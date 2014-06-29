7d768b2af60bb5a3311b405ac9ebe94f65f81b0e
David <davidCdong_123@hotmail.com>
2014-06-29 16:17:48 -0400

    Added Calendar Prototype

--- FlashR/gen/com/example/flashr/R.java
+++ FlashR/gen/com/example/flashr/R.java
@@ -995,13 +995,16 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050050;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int cell_grid=0x7f05003d;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
+        public static final int compoundCalendarView1=0x7f05004b;
         public static final int container=0x7f05003c;
+        public static final int dateText=0x7f05004e;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1010,20 +1013,31 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int friText=0x7f050049;
+        public static final int gridView1=0x7f050043;
+        public static final int headingBar=0x7f05003e;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imageView1=0x7f05004d;
+        public static final int infoText=0x7f05004f;
+        public static final int leftBtn=0x7f05003f;
         public static final int listMode=0x7f050001;
+        public static final int listView1=0x7f050044;
         public static final int list_item=0x7f050029;
+        public static final int list_row=0x7f05004c;
         public static final int middle=0x7f050012;
+        public static final int monText=0x7f050046;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int rightBtn=0x7f050041;
+        public static final int satText=0x7f05004a;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1040,12 +1054,17 @@ containing a value of this type.
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
+        public static final int sunText=0x7f050042;
         public static final int tabMode=0x7f050002;
+        public static final int thurText=0x7f050048;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
+        public static final int tuesText=0x7f050045;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int wedText=0x7f050047;
         public static final int withText=0x7f05000c;
+        public static final int yearMonthDisplay=0x7f050040;
     }
     public static final class integer {
         /**  The maximum number of action buttons that should be permitted within
@@ -1099,10 +1118,13 @@ containing a value of this type.
         public static final int activity_calendar=0x7f030017;
         public static final int activity_create_event=0x7f030018;
         public static final int activity_main=0x7f030019;
-        public static final int fragment_calendar=0x7f03001a;
-        public static final int fragment_create_event=0x7f03001b;
-        public static final int fragment_main=0x7f03001c;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
+        public static final int cell=0x7f03001a;
+        public static final int compound_calendar=0x7f03001b;
+        public static final int fragment_calendar=0x7f03001c;
+        public static final int fragment_create_event=0x7f03001d;
+        public static final int fragment_main=0x7f03001e;
+        public static final int listrow=0x7f03001f;
+        public static final int support_simple_spinner_dropdown_item=0x7f030020;
     }
     public static final class menu {
         public static final int calendar=0x7f0c0000;
@@ -1982,6 +2004,16 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
           @attr name com.example.flashr:textAllCaps
         */
         public static final int CompatTextView_textAllCaps = 0;
+        /** Attributes that can be used with a CompoundCalendarView.
+         */
+        public static final int[] CompoundCalendarView = {
+            
+        };
+        /** Attributes that can be used with a CustomCalendarView.
+         */
+        public static final int[] CustomCalendarView = {
+            
+        };
         /** Attributes that can be used with a LinearLayoutICS.
            <p>Includes the following attributes:</p>
            <table>
--- FlashR/src/com/example/flashr/CalendarActivity.java
+++ FlashR/src/com/example/flashr/CalendarActivity.java
@@ -1,15 +1,26 @@
 package com.example.flashr;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import java.util.Calendar;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.customadaptor.CustomGridViewAdaptor;
+import com.example.customadaptor.CustomListViewAdapter;
 
 public class CalendarActivity extends ActionBarActivity {
 
@@ -22,6 +33,7 @@ public class CalendarActivity extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
 	}
 
 	@Override
@@ -49,6 +61,38 @@ public class CalendarActivity extends ActionBarActivity {
 	 */
 	public static class PlaceholderFragment extends Fragment {
 
+		private int[][] month = new int[12][35];
+		private int[] totalMonthDays = new int[]{
+			31,	28,31,30,31,30,31,31,30,31,30,31
+		};
+		private String[] yearDateList = new String[]{
+				"2014 - January",
+				"2014 - February",
+				"2014 - March",
+				"2014 - April",
+				"2014 - May",
+				"2014 - June",
+				"2014 - July",
+				"2014 - August",
+				"2014 - September",
+				"2014 - October",
+				"2014 - November",
+				"2014 - December",
+		};
+		private String[] listViewInfoItems = new String[]{
+				"First event",
+				"Second Event",
+				"Thrid Event",
+				"Fourth Event"
+		};
+		private String[] listViewDateItems = new String[]{
+				"2014",
+				"2014",
+				"2014",
+				"2014"
+		};
+		private int defaultMonth = 6;
+		private int curMonth = defaultMonth;
 		public PlaceholderFragment() {
 		}
 
@@ -57,8 +101,137 @@ public class CalendarActivity extends ActionBarActivity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_calendar,
 					container, false);
+			populateDays();
+			TextView tv = (TextView)rootView.findViewById(R.id.yearMonthDisplay);
+			
+			tv.setText(yearDateList[defaultMonth]);
+			//int curMonth = getCurMonth(tv);
+			/*GridView gv = (GridView)rootView.findViewById(R.id.gridView1);
+			ListView lv = (ListView)rootView.findViewById(R.id.listView1);
+			
+			
+			CustomGridViewAdaptor gridAdapter = new CustomGridViewAdaptor(getActivity(),month[curMonth]);
+			CustomListViewAdapter listAdapter = new CustomListViewAdapter(getActivity(),listViewDateItems,listViewInfoItems);
+			gv.setAdapter(gridAdapter);
+			lv.setAdapter(listAdapter);*/
+			GridView gv = (GridView)rootView.findViewById(R.id.gridView1);
+			ListView lv = (ListView)rootView.findViewById(R.id.listView1);
+			Button leftBtn = (Button)rootView.findViewById(R.id.leftBtn);
+			Button rightBtn = (Button)rootView.findViewById(R.id.rightBtn);
+			leftBtn.setOnClickListener(new OnClickListener() {
+			    public void onClick(View v)
+			    {
+			        moveDateLeft(v);
+			    } 
+			});
+			rightBtn.setOnClickListener(new OnClickListener() {
+			    public void onClick(View v)
+			    {
+			        moveDateRight(v);
+			    } 
+			});
+			
+			
+			
+			setValues(rootView);
 			return rootView;
 		}
+		private void setValues(final View view){
+			TextView tv = (TextView)view.findViewById(R.id.yearMonthDisplay);
+			GridView gv = (GridView)view.findViewById(R.id.gridView1);
+			ListView lv = (ListView)view.findViewById(R.id.listView1);
+			
+			tv.setText(yearDateList[curMonth]);
+			CustomGridViewAdaptor gridAdapter = new CustomGridViewAdaptor(getActivity(),month[curMonth]);
+			CustomListViewAdapter listAdapter = new CustomListViewAdapter(getActivity(),listViewDateItems,listViewInfoItems);
+			gv.setAdapter(gridAdapter);
+			lv.setAdapter(listAdapter);
+			gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+	            @Override
+	            public void onItemClick(AdapterView<?> parent, View view2, int position, long id) {
+	            	Bundle b = new Bundle();
+	            	Intent intent = new Intent(getActivity(),CreateEventActivity.class);
+	            	
+	            	TextView tv = (TextView)view.findViewById(R.id.yearMonthDisplay);
+	            	//TextView tv = (TextView)parent.getItemAtPosition(position);
+	            	String message = parent.getItemAtPosition(position).toString();
+	            	String yearMonth = tv.getText().toString();
+	            	String year = yearMonth.substring(0,4);
+	            	String month = yearMonth.substring(7);
+	            	String[] data = new String[]{year,month,message};
+	            	b.putStringArray("dateData", data);
+	        		//intent.putExtra(EXTRA_MESSAGE,message);
+	            	intent.putExtras(b);
+	        		
+	        		Log.i("Message",yearMonth);
+	        		startActivity(intent);
+	            }
+	        });
+		}
+		private void populateDays(){
+			Calendar c = Calendar.getInstance();
+			c.set(Calendar.YEAR, 2014);
+			c.set(Calendar.WEEK_OF_YEAR, 1);
+			c.set(Calendar.DAY_OF_YEAR,0);
+			int minDay = c.get(Calendar.DAY_OF_WEEK);
+			int beginDay = 31-(7-minDay)+2;
+			if(beginDay == 0){
+				beginDay++;
+			}
+			for(int curMonth = 0;curMonth<12;curMonth++){
+				int totalDays = totalMonthDays[curMonth]+1;
+				for(int i=0;i<35;i++){
+					month[curMonth][i] = beginDay;
+					beginDay = (beginDay+1)%totalDays;
+					if(beginDay == 0){
+						beginDay++;
+					}
+				}
+				beginDay = (beginDay-7+1+totalDays)%totalDays;
+				if(beginDay == 0){
+					beginDay++;
+				}
+			}
+		}
+		
+		private int getCurMonth(TextView tv){
+			
+			String month = tv.getText().toString().substring(7);
+			if(month.equalsIgnoreCase("January")){
+				return 0;
+			}else if(month.equalsIgnoreCase("February")){
+				return 1;
+			}else if(month.equalsIgnoreCase("March")){
+				return 2;
+			}else if(month.equalsIgnoreCase("April")){
+				return 3;
+			}else if(month.equalsIgnoreCase("May")){
+				return 4;
+			}else if(month.equalsIgnoreCase("June")){
+				return 5;
+			}else if(month.equalsIgnoreCase("July")){
+				return 6;
+			}else if(month.equalsIgnoreCase("August")){
+				return 7;
+			}else if(month.equalsIgnoreCase("September")){
+				return 8;
+			}else if(month.equalsIgnoreCase("October")){
+				return 9;
+			}else if(month.equalsIgnoreCase("November")){
+				return 10;
+			}else{
+				return 11;
+			}
+		}
+		public void moveDateLeft(View view){
+			curMonth--;
+			//setValues(view);
+		}
+		public void moveDateRight(View view){
+			curMonth++;
+			setValues(view);
+		}
 	}
+	
 
 }
