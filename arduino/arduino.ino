6878acf11258bab5a2124c5963124d0a3a98206f
Seth Rubin <seth.h.rubin@gmail.com>
2014-06-29 18:42:46 -0400

    script and arduino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,17 +1,24 @@
-int ledPin = 12;
-
 void setup() {
     Serial.begin(9600);
-    pinMode(ledPin, OUTPUT);
+    pinMode(12, OUTPUT);
 }
+
+int i = 0;
 
 void loop() {
     if (Serial.available() > 0) {
         char value = Serial.read();
-        if (value == '1') {
-            digitalWrite(ledPin, HIGH);
+        if (value == 1) {
+            if (i == 0) {
+                digitalWrite(12, HIGH);
+                i = 1;
+            }
+            else {
+                digitalWrite(12, LOW);
+                i = 0;
+            }
         } else {
-            digitalWrite(ledPin, LOW);
+            digitalWrite(12, LOW);
         }
     }
     delay(1000);
--- send_data.py
+++ send_data.py
@@ -6,14 +6,12 @@ ser = serial.Serial("COM4", 9600)
 time = "0"
 while True:
     if time != str(datetime.datetime.now().time())[:5]:
-        os.system('adb pull //data/data/com.example.flashr/databases/ C:\Users/seth/Desktop')
+        #os.system('adb pull //data/data/com.example.flashr/databases/eventManager C:\Users/seth/Desktop')
+
         time = str(datetime.datetime.now().time())[:5]
         compare_time = time[:2] + time[3:]
-        #file = open('C:\Users/seth/Desktop/event.db', 'r')
+        #file = open('C:\Users/seth/Desktop/eventManager', 'r')
         #string = file.read()
-        if time[-1] == "5":
-            ser.write('1')
-        else:
-            ser.write('0')
+        ser.write('1')
         #print string
         print time
